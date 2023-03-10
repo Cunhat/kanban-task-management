05806fc470e4175bf839f8e06f6114ca154a7fce
Tiago Cunha <tiago92cunha@gmail.com>
2023-03-10 17:27:03 +0000

    Add nexts fonts and Jakarta Plus font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,12 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+const jakarta = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-jakarta",
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +18,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <main className={`${jakarta.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,33 @@
+
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-jakarta)', ...fontFamily.sans],
+      },
+      colors: {
+        customGrey: {
+          100: "#F4F7FD",
+          500: "#828FA3",
+          800: "#2B2C37",
+          900: "#20212C",
+        },
+        lines: {
+          100: "#E4EBFA",
+          900: "#3E3F4E",
+        },
+        primary: "#635FC7",
+        primaryHover: "#A8A4FF",
+        customRed: "#EA5555",
+        customRedHover: "#FF9898",
+
+      }
+    },
   },
   plugins: [],
 };
