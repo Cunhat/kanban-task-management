19e1d4865a043b25e44d2527b580cd716fa534ce
Tiago Cunha <tiago92cunha@gmail.com>
2023-03-11 16:10:11 +0000

    Create Button component

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,39 @@
+import { cva, VariantProps } from "class-variance-authority";
+import { Props as ButtonOrLinkProps, ButtonOrLink } from "./ButtonOrLink";
+
+const buttonStyles = cva("flex items-center justify-center", {
+  variants: {
+    intent: {
+      primary: "bg-primary text-white hover:bg-primaryHover",
+      secondary: "text-primary bg-customGrey-100 hover:bg-customGrey-500",
+      destructive: "text-white bg-customRed hover:bg-customRedHover",
+    },
+    fullWidth: { true: "w-full" },
+    size: {
+      sm: "py-2 px-4 rounded-[20px] text-sm",
+      lg: "py-4 px-6 rounded-3xl text-base",
+    },
+  },
+  defaultVariants: {
+    intent: "primary",
+    size: "lg",
+  },
+});
+
+export interface Props
+  extends ButtonOrLinkProps,
+    VariantProps<typeof buttonStyles> {}
+
+export const Button: React.FC<Props> = ({
+  intent,
+  fullWidth,
+  size,
+  ...props
+}) => {
+  return (
+    <ButtonOrLink
+      className={buttonStyles({ intent, fullWidth, size })}
+      {...props}
+    />
+  );
+};
