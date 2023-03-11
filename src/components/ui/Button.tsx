09a3b580c58c084233f9687496d227378220ab8f
Tiago Cunha <tiago92cunha@gmail.com>
2023-03-11 20:33:03 +0000

    Add disable styles to button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,24 +1,30 @@
 import { cva, VariantProps } from "class-variance-authority";
 import { Props as ButtonOrLinkProps, ButtonOrLink } from "./ButtonOrLink";
 
-const buttonStyles = cva("flex items-center justify-center", {
-  variants: {
-    intent: {
-      primary: "bg-primary text-white hover:bg-primaryHover",
-      secondary: "text-primary bg-customGrey-100 hover:bg-customGrey-500",
-      destructive: "text-white bg-customRed hover:bg-customRedHover",
+const buttonStyles = cva(
+  "flex items-center justify-center hover:cursor-pointer disabled:cursor-not-allowed",
+  {
+    variants: {
+      intent: {
+        primary:
+          "bg-primary text-white hover:bg-primaryHover disabled:bg-primaryHover",
+        secondary:
+          "text-primary bg-customGrey-100 hover:bg-customGrey-500 disabled:bg-customGrey-500",
+        destructive:
+          "text-white bg-customRed hover:bg-customRedHover disabled:bg-customRedHover",
+      },
+      fullWidth: { true: "w-full" },
+      size: {
+        sm: "py-2 px-4 rounded-[20px] text-sm",
+        lg: "py-3 px-6 rounded-3xl text-base",
+      },
     },
-    fullWidth: { true: "w-full" },
-    size: {
-      sm: "py-2 px-4 rounded-[20px] text-sm",
-      lg: "py-4 px-6 rounded-3xl text-base",
+    defaultVariants: {
+      intent: "primary",
+      size: "lg",
     },
-  },
-  defaultVariants: {
-    intent: "primary",
-    size: "lg",
-  },
-});
+  }
+);
 
 export interface Props
   extends ButtonOrLinkProps,
